8695ddb9bcf818a3a0f6a86ada8aa34fcb8cb5da
QuasarDev <quasardev9@gmail.com>
2026-09-23 12:03:57 +0000

    feat(users): add endpoint to get the profile of other users along with their playlists

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,10 +1,25 @@
-import { getProfile } from "./user.service.js";
+import { getProfile, getPublicProfile } from "./user.service.js";
 
 export async function profile(req, res, next) {
   try {
     const { id } = req.user;
     const user = await getProfile(id);
   
+    return res.status(200).json({
+      success: true,
+      message: 'User profile',
+      data: user
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function publicProfile(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user = await getPublicProfile(id);
+
     return res.status(200).json({
       success: true,
       message: 'User profile',
