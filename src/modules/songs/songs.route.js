de5fe386ed8c6d781a1f88cc50f44b1c1d73f6fc
QuasarDev <quasardev9@gmail.com>
2026-09-23 14:35:47 +0000

    feat(songs): add endpoint to return necessary details for creating a link to share songs

--- src/modules/songs/songs.route.js
+++ src/modules/songs/songs.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { authenticate } from "../../shared/middleware/auth.middleware.js";
-import { songs, search, songInfo, stream } from "./songs.controller.js";
+import { songs, search, songInfo, stream, share } from "./songs.controller.js";
 
 const router = Router();
 router.use(authenticate);
 
 router.get('/', songs);
 router.get('/search', search);
-router.get('/:id', songInfo);
 router.get('/:id/stream', stream);
+router.post('/:id/share', share);
+router.get('/:id', songInfo);
 
 export default router;
